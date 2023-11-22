1e089f82c6734623e8ca4725f64937d114396a64
Andy <handy_4ndy@outlook.com>
2023-11-22 20:26:39 +0000

    feedback.js changes

--- Assets/Scripts/feedback.js
+++ Assets/Scripts/feedback.js
@@ -1,17 +1,23 @@
+// Get references to the elements
 const formSection = document.getElementById('formSection');
+const feedbackForm = document.getElementById('feedbackForm');
+const youthFeedback = document.getElementById('youthFeedback');
+const volunteerForm = document.getElementById('volunteerForm');
 
+// Initially hide the form section and all iframes
+formSection.style.display = 'none';
+feedbackForm.style.display = 'none';
+youthFeedback.style.display = 'none';
+volunteerForm.style.display = 'none';
 
-
-
+// Function to show the selected form
 function showForm(formId) {
     // Hide all iframes
-    document.getElementById('feedbackForm').style.display = 'none';
-    document.getElementById('youthFeedback').style.display = 'none';
-    document.getElementById('volunteerForm').style.display = 'none';
+    feedbackForm.style.display = 'none';
+    youthFeedback.style.display = 'none';
+    volunteerForm.style.display = 'none';
 
-    // Show the selected iframe
+    // Show the selected iframe and the form section
     formSection.style.display = 'block';
     document.getElementById(formId).style.display = 'block';
 }
-
-formSection.style.display = 'none';
