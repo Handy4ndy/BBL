e547df60ea61a174685c7952fcfc0bb0857cecbc
Andy <handy_4ndy@outlook.com>
2023-11-22 20:36:50 +0000

    feedback.js changes

--- Assets/Scripts/feedback.js
+++ Assets/Scripts/feedback.js
@@ -1,23 +1,28 @@
-// Get references to the elements
-const formSection = document.getElementById('formSection');
-const feedbackForm = document.getElementById('feedbackForm');
-const youthFeedback = document.getElementById('youthFeedback');
-const volunteerForm = document.getElementById('volunteerForm');
+document.addEventListener('DOMContentLoaded', function () {
+    // Get references to the elements
+    const formSection = document.getElementById('formSection');
+    const feedbackForm = document.getElementById('feedbackForm');
+    const youthFeedback = document.getElementById('youthFeedback');
+    const volunteerForm = document.getElementById('volunteerForm');
 
-// Initially hide the form section and all iframes
-formSection.style.display = 'none';
-feedbackForm.style.display = 'none';
-youthFeedback.style.display = 'none';
-volunteerForm.style.display = 'none';
+    // Check if elements are found before manipulating
+    if (formSection && feedbackForm && youthFeedback && volunteerForm) {
+        // Initially hide the form section and all iframes
+        formSection.style.display = 'none';
+        feedbackForm.style.display = 'none';
+        youthFeedback.style.display = 'none';
+        volunteerForm.style.display = 'none';
 
-// Function to show the selected form
-function showForm(formId) {
-    // Hide all iframes
-    feedbackForm.style.display = 'none';
-    youthFeedback.style.display = 'none';
-    volunteerForm.style.display = 'none';
+        // Function to show the selected form
+        window.showForm = function (formId) {
+            // Hide all iframes
+            feedbackForm.style.display = 'none';
+            youthFeedback.style.display = 'none';
+            volunteerForm.style.display = 'none';
 
-    // Show the selected iframe and the form section
-    formSection.style.display = 'block';
-    document.getElementById(formId).style.display = 'block';
-}
+            // Show the selected iframe and the form section
+            formSection.style.display = 'block';
+            document.getElementById(formId).style.display = 'block';
+        }
+    }
+});
